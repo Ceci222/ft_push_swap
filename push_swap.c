a620d47f21cbd16dcb7971202cc63690568624bc
Cecilia Scaroni <cscaroni@c3r3s3.42urduliz.com>
2026-02-23 18:12:03 +0100

    Attempt at validating data

--- push_swap.c
+++ push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lajen-li <lajen-li@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cscaroni <cscaroni@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 11:49:11 by lajen-li          #+#    #+#             */
-/*   Updated: 2026/02/23 17:08:50 by lajen-li         ###   ########.fr       */
+/*   Updated: 2026/02/23 18:10:18 by cscaroni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,34 +17,43 @@ void push_swap(int argc, char **argv)
 	int	arg_num;
 	char **clean_array;
 	int i;
-	
+	t_list	*clean_numbers;
+
 	i = 1;
 	if (!argv)
 		return ;
 	while (i < argc)
 	{
-		if (ft_alpha(argv[i]) == 1)
+/* 		if (ft_alpha(argv[i]) == 1)
 		{
 			write(2, "Error", 5);
 			write(2, "\n", 1);
 			return ;
-		}
+		} */
 		if (ft_isdigit(argv[i]) == 0)
 		{
 			if (argv[i] == '+' || argv[i] == '-')
-			
+			{
 				if (argv[i + 1] == '+' || argv[i + 1] == '-')
 				{
 					write(2, "Error", 5);
 					write(2, "\n", 1);
 					return ;
 				}
+			}
+			else
+			{
+				write(2, "Error", 5);
+				write(2, "\n", 1);
+				return ;
+			}
+		}
+		else
+		{
+			clean_numbers = ft_lstnew(ft_atoi(argv)); 
 		}
 		i++;
 	}
-	
-	
-	
 	clean_array = ft_split(argv[1], " ");
 	while (clean_array[i])
 	{
