dd7b9c3f188d4b6bc23446669ef4e37b33a9500a
Lesly Janet Ajen Licas <lajen-li@c3r5s1.42urduliz.com>
2026-03-20 13:45:47 +0100

    create convert function to optimize ft_fill_stack in ps_parser.c

--- ps_parser.c
+++ ps_parser.c
@@ -14,14 +14,12 @@ static int read_flags(char *argv)
 		return (2);
 	return (0);
 }
-char	**ft_parser(int argc, char **argv)
+char	**ft_parser(int argc, char **argv, int i)
 {
-	int		i;
 	char	*temp;
 	char	**final_data;
 	char	*full_string;
 
-	i = 1;
 	if (argc == 2)
 		full_string = ft_strdup(argv[i]);
 	else
@@ -44,17 +42,29 @@ char	**ft_parser(int argc, char **argv)
 	final_data = ft_split(full_string, ' ');
 	return (free(full_string), final_data);
 }
+static void	convert (char *arg, t_stack *stack_a)
+{
+	long num;
+	t_node *number_node;
+
+	num = ft_atol(arg);
+	if (num > 2147483647 || num < -2147483648)
+		return (ft_free_stack(stack_a), ft_error(), (void)(0));
+	if (is_duplicate(stack_a, num))
+		return (ft_free_stack(stack_a), (void)(0));
+	number_node = ft_create_node(num);
+	ft_node_to_bottom(stack_a, number_node -> content);
+	free(number_node);
+}
 
 t_stack	*ft_fill_stack(char **arg)
 {
 	int		i;
-	long	num;
-	t_node	*number_node;
 	t_stack	*stack_a;
 
 	stack_a = ft_create_stack();
 	i = 0;
-	if (read_flags(arg[i]) == 1)
+	 if (read_flags(arg[i]) == 1)
 		i++;
 	else if (read_flags(arg[i]) == 2)
 		i++;
@@ -63,16 +73,7 @@ t_stack	*ft_fill_stack(char **arg)
 	while (arg[i])
 	{
 		if (is_right_number(arg[i]))
-		{
-			num = ft_atol(arg[i]);
-			if (num > 2147483647 || num < -2147483648)
-				return (ft_free_stack(stack_a), ft_error(), NULL);
-			if (is_duplicate(stack_a, num))
-				return (ft_free_stack(stack_a), NULL);
-			number_node = ft_create_node(num);
-			ft_node_to_bottom(stack_a, number_node -> content);
-			free(number_node);
-		}
+			convert(arg[i], stack_a);
 		else
 			return (ft_free_stack(stack_a), ft_error(), NULL);
 		i++;
